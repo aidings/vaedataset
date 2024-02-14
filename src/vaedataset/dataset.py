a122f9b244313a1f30e555c7308ba594a4264404
aidings <ioimage@163.com>
2024-02-14 15:35:29 +0800

    version 0.0.2 : remove InjectImageDataset

--- src/vaedataset/dataset.py
+++ src/vaedataset/dataset.py
@@ -1,15 +1,18 @@
-from bdataset import InjectImageDataset
+from bdataset import InjectDataset
 from imgaug.specs import VaeTransform
+from imgaug import to_pil
 from PIL import Image
 
 
-class InjectVaeDataset(InjectImageDataset):
+class InjectVaeDataset(InjectDataset):
     def __init__(self, size=256, img_key='img_path'):
-        super().__init__(img_key)
+        super().__init__()
+        self.img_key = img_key
         self.trans = VaeTransform(size, resample=Image.BILINEAR)
     
     def transforms(self, idx):
-        image = self.imread(idx)
+        image = to_pil(self.datas[idx][self.img_key])
+        image = image.convert('RGB')
         return self.trans(image)
     
 
